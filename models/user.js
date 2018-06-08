677fd154ed8ef03720303219daf1e1a3162c8e4e
Barakat Turki <barakat.turki@gmail.com>
2018-06-08 20:06:21 +0200

    create User model

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    required: true,
+    type: String
+  },
+  password: {
+    default: null,
+    type: String
+  },
+  fullname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    default: null
+  },
+  title: {
+    type: String,
+    default: null
+  },
+  sallary: {
+    type: String,
+    default: null
+  },
+  addedBy: {
+    type: mongoose.SchemaTypes.ObjectId ,
+    ref: 'User',
+    default: null
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
