6ee915490b6f30fce446ca02dc8b19cd14585b07
Barakat Turki <barakat.turki@gmail.com>
2018-06-16 06:40:55 +0200

    removed the api docs from the root

--- api_doc/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "url": "https://msn-hr-system.herokuapp.com/api/v1",  "name": "HR system Back-End API",  "version": "1.0.0",  "description": "not proviede yet",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-06-16T03:46:02.169Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
