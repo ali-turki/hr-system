77ffca835e440940c2edfc5c9f2ff45367d193e0
Barakat Turki <barakat.turki@gmail.com>
2018-06-16 06:41:35 +0200

    support production db

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const config = require('../config');
-mongoose.connect(`${config.DB.HOST}/${config.DB.NAME}`)
+
+let mongodbConnection = `${config.DB.HOST}/${config.DB.NAME}`;
+if (config.ENV.MODE === 'production') {
+  mongodbConnection = 'mongodb://aliturki:asdqwe123@ds259820.mlab.com:59820/hr-system';
+}
+
+mongoose.connect(mongodbConnection)
   .then(result => {
-    console.log(`mongodb runing on ${config.DB.HOST}/${config.DB.NAME}`);
+    console.log(`mongodb runing on the ${config.ENV.MODE} env...`);
   })
   .catch(err => {
     console.log(err.message);
